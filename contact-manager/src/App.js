e17d1c7f7818e9d67c12daf94ce2ebd857a03aae
Everton Pereira <everton.mwp@gmail.com>
2019-01-12 03:23:16 -0200

    added bootstrap

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="Everton Pereira" email="emwp@gmail.com" phone="+5547999573901" />
+          <Contact name="Douglas" email="dogla@gmail.com" phone="+5547991588893" />
+        </div>
       </div>
     );
   }
