f2252370680290acd1480dea7813e41b0c5f4215
Everton Pereira <everton.mwp@gmail.com>
2019-01-13 01:44:30 -0200

    added state features to buttons

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
 import Header from './components/Header';
+import Contacts from './components/Contacts';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
@@ -9,8 +10,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact name="Everton Pereira" email="emwp@gmail.com" phone="+5547999573901" />
-          <Contact name="Douglas" email="dogla@gmail.com" phone="+5547991588893" />
+          <Contacts />
         </div>
       </div>
     );
