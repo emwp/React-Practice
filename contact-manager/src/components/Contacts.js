1140e052cb5caaef8b3e541242fffd184ba8c743
Everton Pereira <everton.mwp@gmail.com>
2019-01-14 23:10:39 -0200

    context api to manage state

--- contact-manager/src/components/Contacts.js
+++ contact-manager/src/components/Contacts.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'Douglas',
-        email: 'dorgla@gmail.com',
-        phone: '+5547999655544',
-      },
-      {
-        id: 2,
-        name: 'Osvaldo',
-        email: 'osvaldo@gmail.com',
-        phone: '+55479996544785',
-      },
-      {
-        id: 3,
-        name: 'Jonatas',
-        email: 'jonatas@gmail.com',
-        phone: '+55479996544781',
-      },
-    ],
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
---
+++ contact-manager/src/context.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Douglas',
+        email: 'dorgla@gmail.com',
+        phone: '+5547999655544',
+      },
+      {
+        id: 2,
+        name: 'Osvaldo',
+        email: 'osvaldo@gmail.com',
+        phone: '+55479996544785',
+      },
+      {
+        id: 3,
+        name: 'Jonatas',
+        email: 'jonatas@gmail.com',
+        phone: '+55479996544781',
+      },
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    },
+  };
+  render() {
+    return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
+  }
+}
+
+export const Consumer = Context.Consumer;
