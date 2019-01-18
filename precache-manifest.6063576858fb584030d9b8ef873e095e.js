1ae1cf34f56b863906572786a0b204d55a617194
Everton Pereira <everton.mwp@gmail.com>
2019-01-17 22:04:52 -0200

    Updates

---
+++ precache-manifest.6063576858fb584030d9b8ef873e095e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "beff4edeb010f94998ef",
+    "url": "/React-Practice/static/js/runtime~main.beff4ede.js"
+  },
+  {
+    "revision": "dae3f75865fa8e40dab7",
+    "url": "/React-Practice/static/js/main.dae3f758.chunk.js"
+  },
+  {
+    "revision": "d9e45468afd6d61f9a30",
+    "url": "/React-Practice/static/js/1.d9e45468.chunk.js"
+  },
+  {
+    "revision": "d9e45468afd6d61f9a30",
+    "url": "/React-Practice/static/css/1.26962566.chunk.css"
+  },
+  {
+    "revision": "17ccd3770ce771c09128fb7bb3070514",
+    "url": "/React-Practice/index.html"
+  }
+];
